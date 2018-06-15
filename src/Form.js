e61f7e4b89d22e51323d5932a4a89a511cf9d0fe
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-06-15 15:28:16 -0400

    styled form page

--- src/Form.js
+++ src/Form.js
@@ -27,53 +27,55 @@ class Form extends Component {
   };
   render() {
     return (
-      <div className="form-page">
-        {/* <form onSubmit={this.handleCharge}> */}
-        <div className="form-text">
-          <label>Name</label>
-          <input
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-            onChange={e => this.handleChange(e)}
-            autoFocus
-          />
-        </div>
-        <div className="form-text">
-          <label>Address</label>
-          <input
-            placeholder="address"
-            value={this.state.address}
-            name="address"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <div className="form-text">
-          <label>City</label>
-          <input
-            placeholder="city"
-            value={this.state.city}
-            name="city"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <div className="form-text">
-          <label>State</label>
-          <input
-            placeholder="state"
-            value={this.state.st}
-            name="st"
-            onChange={e => this.handleChange(e)}
-          />
-        </div>
-        <div className="form-text">
-          <label>Zipcode</label>
-          <input
-            placeholder="zipcode"
-            value={this.state.zip}
-            name="zip"
-            onChange={e => this.handleChange(e)}
-          />
+      <div className="form-outer">
+        <div className="form-page">
+          {/* <form onSubmit={this.handleCharge}> */}
+          <div className="form-text">
+            <label>Name</label>
+            <input
+              placeholder="name"
+              value={this.state.name}
+              name="name"
+              onChange={e => this.handleChange(e)}
+              autoFocus
+            />
+          </div>
+          <div className="form-text">
+            <label>Address</label>
+            <input
+              placeholder="address"
+              value={this.state.address}
+              name="address"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="form-text">
+            <label>City</label>
+            <input
+              placeholder="city"
+              value={this.state.city}
+              name="city"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="form-text">
+            <label>State</label>
+            <input
+              placeholder="state"
+              value={this.state.st}
+              name="st"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="form-text">
+            <label>Zipcode</label>
+            <input
+              placeholder="zipcode"
+              value={this.state.zip}
+              name="zip"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
         </div>
         <button onClick={() => this.handleCharge()}>Submit</button>
         {/* </form> */}
